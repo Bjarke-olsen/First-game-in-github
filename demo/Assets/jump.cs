c8dd4045d13b696931267e853704d04414117fea
Bjarke <bjar3059@h-u.dk>
2019-08-28 15:06:00 +0200

    added unity procject

---
+++ demo/Assets/jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    [SerializeField]
+    private float forcemod = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // hvis man trykker sker der noget
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * forcemod, ForceMode2D.Impulse);
+        }
+    }
+}
